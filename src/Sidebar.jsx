2d8b87628a96f22ddaf439d6eef7c9de5edbded9
Manuel Manriquez <manriquezmanny@gmail.com>
2024-02-24 01:01:52 -0800

    Finished basic front end, should probably start working on database now.

--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -16,12 +16,31 @@ function Sidebar(props) {
         <div className={toggleState ? "sidebar active" : "sidebar"}>
             {toggleState ?
                 <div className="sidebar-header active">
-                    <button onClick={toggleSideBar} className={"sidebar-btn active"}><img src="../public/listLeap.png" className="logo"></img></button>
-                    <h1 className="app-name">ListLeap</h1>
+                    <button onClick={toggleSideBar} className="sidebar-btn active"><img src="/listLeap.png" className="logo"></img></button>
+                    <h1 className="app-name">LeapList</h1>
                 </div>
                 : 
                 <div className="sidebar-header">
-                    <button onClick={toggleSideBar} className={"sidebar-btn"}><img src="../public/listLeap.png" className="logo"></img></button>
+                    <button onClick={toggleSideBar} className="sidebar-btn"><img src="/listLeap.png" className="logo"></img></button>
+                </div>
+            }
+
+            {toggleState && 
+                <div className="user-lists"></div>
+            }
+            {toggleState ? 
+                <div className="login active">
+                    <div className="login active">
+                        <button onClick={toggleSideBar} className="login-btn"><img src="/login.png" className="login-icon"></img></button>
+                        <p>Username1234!</p>
+                    </div>
+                    <div className="log-out-div">
+                        <button className="log-out-btn">Log Out</button>
+                    </div>
+                </div>
+                : 
+                <div className="login">
+                    <button onClick={toggleSideBar} className="login-btn"><img src="/login.png" className="login-icon"></img></button>
                 </div>
             }
         </div>
